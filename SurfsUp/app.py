61ea937577f5d74a93e4a880fe9b4534e79c0980
Jenni <jenniferjaurequi7@gmail.com>
2024-08-11 15:08:38 -0700

    climate code analysis work & app draft

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,39 @@
 # Import the dependencies.
-
-
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import func
 
 #################################################
 # Database Setup
 #################################################
 
-
 # reflect an existing database into a new model
+engine = create_engine("sqlite:///SurfsUp/Resources/hawaii.sqlite")
 
 # reflect the tables
-
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+Base.classes.keys()
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+import flask from Flask
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+
+
